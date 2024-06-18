6500d4723ba707835ea31f2f63f82dd9679e0173
CV2 CV2 <2cv36985@gmail.com>
2024-06-18 13:20:33 -0500

    modifico AdminHomePage

--- lib/src/presentation/pages/admin/home/AdminHomePage.dart
+++ lib/src/presentation/pages/admin/home/AdminHomePage.dart
@@ -1,4 +1,7 @@
+import 'package:ecommerce_flutter/src/presentation/pages/admin/home/bloc/AdminHomeBloc.dart';
+import 'package:ecommerce_flutter/src/presentation/pages/admin/home/bloc/AdminHomeState.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AdminHomePage extends StatefulWidget {
   const AdminHomePage({super.key});
@@ -11,8 +14,11 @@ class _AdminHomePageState extends State<AdminHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Text('AdminHomePage'),
-    ));
+        body: BlocBuilder<AdminHomeBloc, AdminHomeState>(
+      builder: (context, state){
+        
+      }
+    )
+    );
   }
 }
