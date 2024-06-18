c7077f443de066e63e5b9740315c93f351fff426
CV2 CV2 <2cv36985@gmail.com>
2024-06-18 11:35:09 -0500

    Agrego las nuevas dependecias en injection.config

--- lib/injection.config.dart
+++ lib/injection.config.dart
@@ -8,13 +8,15 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:ecommerce_flutter/src/data/dataSource/local/SharedPref.dart'
+    as _i4;
 import 'package:ecommerce_flutter/src/data/dataSource/remote/services/AuthService.dart'
     as _i3;
-import 'package:ecommerce_flutter/src/di/AppModule.dart' as _i6;
+import 'package:ecommerce_flutter/src/di/AppModule.dart' as _i7;
 import 'package:ecommerce_flutter/src/domain/repository/AuthRepository.dart'
-    as _i4;
-import 'package:ecommerce_flutter/src/domain/useCase/auth/AuthUseCases.dart'
     as _i5;
+import 'package:ecommerce_flutter/src/domain/useCase/auth/AuthUseCases.dart'
+    as _i6;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
@@ -31,10 +33,11 @@ extension GetItInjectableX on _i1.GetIt {
     );
     final appModule = _$AppModule();
     gh.factory<_i3.AuthService>(() => appModule.authService);
-    gh.factory<_i4.AuthRepository>(() => appModule.authRepository);
-    gh.factory<_i5.AuthUseCases>(() => appModule.authUseCases);
+    gh.factory<_i4.SharedPref>(() => appModule.sharedPref);
+    gh.factory<_i5.AuthRepository>(() => appModule.authRepository);
+    gh.factory<_i6.AuthUseCases>(() => appModule.authUseCases);
     return this;
   }
 }
 
-class _$AppModule extends _i6.AppModule {}
+class _$AppModule extends _i7.AppModule {}
--- lib/src/presentation/pages/auth/login/bloc/LoginBloc.dart
+++ lib/src/presentation/pages/auth/login/bloc/LoginBloc.dart
@@ -35,7 +35,7 @@ class LoginBloc extends Bloc<LoginEvent,LoginState> {
   }
 
 Future<void> _onInitialEvent(InitialEvent event,Emitter<LoginState> emit) async{
-  AuthResponse? authResponse = authUseCases.getUserSession.run();
+  AuthResponse? authResponse = await authUseCases.getUserSession.run();
   emit(state.copyWith(formKey: formKey));
 }
 Future<void> _onEmailChange(EmailChange event,Emitter<LoginState>emit) async{
