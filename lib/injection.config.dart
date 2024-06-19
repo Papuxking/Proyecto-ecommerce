60f642ca8b26d2eadb88a14f50ff42cc3b335915
CV2 CV2 <2cv36985@gmail.com>
2024-06-19 16:39:46 -0500

    Inyecto nuevas dependencias

--- lib/injection.config.dart
+++ lib/injection.config.dart
@@ -9,26 +9,32 @@
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:ecommerce_flutter/src/data/dataSource/local/SharedPref.dart'
-    as _i7;
+    as _i9;
 import 'package:ecommerce_flutter/src/data/dataSource/remote/services/AuthService.dart'
     as _i3;
 import 'package:ecommerce_flutter/src/data/dataSource/remote/services/CategoriesService.dart'
     as _i6;
+import 'package:ecommerce_flutter/src/data/dataSource/remote/services/ProductsService.dart'
+    as _i7;
 import 'package:ecommerce_flutter/src/data/dataSource/remote/services/UsersService.dart'
     as _i4;
-import 'package:ecommerce_flutter/src/di/AppModule.dart' as _i13;
+import 'package:ecommerce_flutter/src/di/AppModule.dart' as _i16;
 import 'package:ecommerce_flutter/src/domain/repository/AuthRepository.dart'
-    as _i9;
+    as _i11;
 import 'package:ecommerce_flutter/src/domain/repository/CategoriesRepository.dart'
+    as _i10;
+import 'package:ecommerce_flutter/src/domain/repository/ProductsRepository.dart'
     as _i8;
 import 'package:ecommerce_flutter/src/domain/repository/UsersRepository.dart'
     as _i5;
 import 'package:ecommerce_flutter/src/domain/useCases/auth/AuthUseCases.dart'
-    as _i10;
-import 'package:ecommerce_flutter/src/domain/useCases/categories/CategoriesUseCases.dart'
     as _i12;
+import 'package:ecommerce_flutter/src/domain/useCases/categories/CategoriesUseCases.dart'
+    as _i14;
+import 'package:ecommerce_flutter/src/domain/useCases/products/ProductsUseCases.dart'
+    as _i15;
 import 'package:ecommerce_flutter/src/domain/useCases/users/UsersUseCases.dart'
-    as _i11;
+    as _i13;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
@@ -49,14 +55,17 @@ extension GetItInjectableX on _i1.GetIt {
     gh.factory<_i4.UsersService>(() => appModule.usersService);
     gh.factory<_i5.UsersRepository>(() => appModule.usersRepository);
     gh.factory<_i6.CategoriesService>(() => appModule.categoriesService);
-    gh.factory<_i7.SharedPref>(() => appModule.sharedPref);
-    gh.factory<_i8.CategoriesRepository>(() => appModule.categoriesRepository);
-    gh.factory<_i9.AuthRepository>(() => appModule.authRepository);
-    gh.factory<_i10.AuthUseCases>(() => appModule.authUseCases);
-    gh.factory<_i11.UsersUseCases>(() => appModule.usersUseCases);
-    gh.factory<_i12.CategoriesUseCases>(() => appModule.categoriesUseCases);
+    gh.factory<_i7.ProductsService>(() => appModule.productsService);
+    gh.factory<_i8.ProductsRepository>(() => appModule.productsRepository);
+    gh.factory<_i9.SharedPref>(() => appModule.sharedPref);
+    gh.factory<_i10.CategoriesRepository>(() => appModule.categoriesRepository);
+    gh.factory<_i11.AuthRepository>(() => appModule.authRepository);
+    gh.factory<_i12.AuthUseCases>(() => appModule.authUseCases);
+    gh.factory<_i13.UsersUseCases>(() => appModule.usersUseCases);
+    gh.factory<_i14.CategoriesUseCases>(() => appModule.categoriesUseCases);
+    gh.factory<_i15.ProductsUseCases>(() => appModule.productsUseCases);
     return this;
   }
 }
 
-class _$AppModule extends _i13.AppModule {}
+class _$AppModule extends _i16.AppModule {}
