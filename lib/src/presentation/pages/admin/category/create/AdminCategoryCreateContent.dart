0a55388298e217cea358ebabe1d56f4dfe92bc3d
CV2 CV2 <2cv36985@gmail.com>
2024-06-19 09:46:19 -0500

    Agrego algunos widgets

--- lib/src/presentation/pages/admin/category/create/AdminCategoryCreateContent.dart
+++ lib/src/presentation/pages/admin/category/create/AdminCategoryCreateContent.dart
@@ -1,5 +1,8 @@
 import 'package:ecommerce_flutter/src/presentation/pages/admin/category/create/bloc/AdminCategoryCreateBloc.dart';
 import 'package:ecommerce_flutter/src/presentation/pages/admin/category/create/bloc/AdminCategoryCreateState.dart';
+import 'package:ecommerce_flutter/src/presentation/utils/BlocFormItem.dart';
+import 'package:ecommerce_flutter/src/presentation/utils/SelectOptionImageDialog.dart';
+import 'package:ecommerce_flutter/src/presentation/widgets/DefaultTextField.dart';
 import 'package:flutter/material.dart';
 
 class AdminCategoryCreateContent extends StatelessWidget {
@@ -15,4 +18,54 @@ class AdminCategoryCreateContent extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container();
   }
+
+  Widget _cardCategoryForm(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: MediaQuery.of(context).size.height * 0.42,
+      decoration: BoxDecoration(
+        color: Color.fromRGBO(255, 255, 255, 0.7),
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(35),
+          topRight: Radius.circular(35),
+        )
+      ),
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 35),
+        child: Column(
+          children: [
+            _textNewCategory(),
+          ],
+        ),
+      ),
+    );
+  } 
+
+  
+
+  Widget _textNewCategory() {
+    return Container(
+      alignment: Alignment.centerLeft,
+      margin: EdgeInsets.only(top: 35, left: 10, bottom: 10),
+      child: Text(
+        'NUEVA CATEGORIA',
+        style: TextStyle(
+          fontSize: 17
+        ),
+      ),
+    );
+  }
+
+
+  Widget _imageBackground(BuildContext context) {
+    return Image.asset(
+      'assets/img/background4.jpg',
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      fit: BoxFit.cover,
+      color: Color.fromRGBO(0, 0, 0, 0.7),
+      colorBlendMode: BlendMode.darken,
+    );
+  }
+
 }
