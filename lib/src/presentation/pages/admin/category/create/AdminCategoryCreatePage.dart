fc315df4416c55d899d5513c8af1fe9602342990
CV2 CV2 <2cv36985@gmail.com>
2024-06-19 09:30:48 -0500

    Agrego los archivos plantilla para Admin Category

---
+++ lib/src/presentation/pages/admin/category/create/AdminCategoryCreatePage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AdminCategoryCreatePage extends StatelessWidget {
+  const AdminCategoryCreatePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
