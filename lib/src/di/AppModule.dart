9dfe5570ab924bc3d9de037f9b98bbaafec39d6a
CV2 CV2 <2cv36985@gmail.com>
2024-06-19 10:40:55 -0500

    Agrego Categories en AppModule

--- lib/src/di/AppModule.dart
+++ lib/src/di/AppModule.dart
@@ -1,11 +1,14 @@
 // src/di/AppModule.dart
 import 'package:ecommerce_flutter/src/data/dataSource/local/SharedPref.dart';
+import 'package:ecommerce_flutter/src/data/dataSource/remote/services/CategoriesService.dart';
 import 'package:ecommerce_flutter/src/data/dataSource/remote/services/UsersService.dart';
 import 'package:ecommerce_flutter/src/data/repository/AuthRepositoryImpl.dart';
 import 'package:ecommerce_flutter/src/data/dataSource/remote/services/AuthService.dart';
+import 'package:ecommerce_flutter/src/data/repository/CategoriesRepositoryImpl.dart';
 import 'package:ecommerce_flutter/src/data/repository/UsersRepositoryImpl.dart';
 import 'package:ecommerce_flutter/src/domain/models/AuthResponse.dart';
 import 'package:ecommerce_flutter/src/domain/repository/AuthRepository.dart';
+import 'package:ecommerce_flutter/src/domain/repository/CategoriesRepository.dart';
 import 'package:ecommerce_flutter/src/domain/repository/UsersRepository.dart';
 import 'package:ecommerce_flutter/src/domain/useCases/auth/AuthUseCases.dart';
 import 'package:ecommerce_flutter/src/domain/useCases/auth/GetUserSessionUseCase.dart';
@@ -41,8 +44,14 @@ abstract class AppModule{
   @injectable
   UsersRepository get usersRepository => UsersRepositoryImpl(usersService);
 
+ @injectable
+  CategoriesService get categoriesService => CategoriesService(token);
+
   @injectable
   SharedPref get sharedPref => SharedPref();
+
+@injectable
+  CategoriesRepository get categoriesRepository => CategoriesRepositoryImpl(categoriesService);
 
   @injectable
   AuthRepository get authRepository => AuthRepositoryImpl(authService, sharedPref);
