bf2a52061e86e1e0df6d328213451662aebf8f5d
CV2 CV2 <2cv36985@gmail.com>
2024-06-22 12:04:43 -0500

    Creo Clientpaymentstatuscontent

---
+++ lib/src/presentation/pages/client/payment/status/ClientPaymentStatusContent.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Clientpaymentstatuscontent extends StatefulWidget {
+  const Clientpaymentstatuscontent({super.key});
+
+  @override
+  State<Clientpaymentstatuscontent> createState() => _ClientpaymentstatuscontentState();
+}
+
+class _ClientpaymentstatuscontentState extends State<Clientpaymentstatuscontent> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [],
+    )
+  }
+}
